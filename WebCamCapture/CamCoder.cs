3abe7920724983f05bc07e69d854e33d222fe932
Martin Gravråk <martin.gravrak@bouvet.no>
2016-04-02 09:42:25 +0200

    kinda working

---
+++ WebCamCapture/CamCoder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CamCaptureLib
+{
+    class CamCoder
+    {
+        CamCam camcam = new CamCam();
+
+        public CamCoder()
+        {
+            camcam.Start();
+        }
+
+
+        public string BuildFailed()
+        {
+
+            return "";
+        }
+
+    }
+}
