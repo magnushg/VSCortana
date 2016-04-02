71bd7f90d75f2d9ec3f2a87e5d5ebf3b633b3115
margrav@gmail.com <martin.gravrak@bouvet.no>
2016-04-02 14:07:23 +0200

    minor updates

--- WebCamCapture/EmotionResult.cs
+++ WebCamCapture/EmotionResult.cs
@@ -22,20 +22,25 @@ namespace CamCaptureLib
             {
                 if (Emotions.First().Scores.Surprise > 0.5)
                 {
-                    return "Dont look so supprised, you broke the build!";
+                    return "I'm just as suprised as you, but you broke the build!";
                 }
                 if (Emotions.First().Scores.Fear > 0.5)
                 {
+                    //Relax, its only ONES and ZEROS !
                     return "Dont worry.. nobody will notice you broke the build!";
                 }
                 if (Emotions.First().Scores.Anger > 0.5)
                 {
+                    //For every minute you remain angry, you give up sixty seconds of peace of mind.
+                    //When anger rises, think of the consequences.
+                    //Anger is a momentary madness, so control your passion or it will control you.
+                    //There's nothing wrong with anger provided you use it constructively.
                     return "I know how you feel!";
                 }
 
                 if (Emotions.First().Scores.Disgust > 0.5)
                 {
-                    return "Dont worry.. nobody will notice you broke the build!";
+                    return "Don’t worry if it doesn’t work right. If everything did, you’d be out of a job.";
                 }
 
                 if (Emotions.First().Scores.Happiness > 0.5)
@@ -56,12 +61,27 @@ namespace CamCaptureLib
 
         public string BuildOK()
         {
-            return "Way to go!";
+            //In my experience there is no such thing as luck.
+
+            return "Hey! It compiles! Ship it!";
         }
 
         public string RunTests()
         {
+
             return "Well done!";
+        }
+
+        public string Exception()
+        {
+            return
+                "The great thing about Object Oriented code is that it can make small, simple problems look like large, complex ones";
+            //Don’t fix bugs later; fix them now.
+            //Never allow the same bug to bite you twice.
+            //"Engineers call them edge cases. I call them: what our users do.”
+            // The world is coming to an end... SAVE YOUR BUFFERS !"
+
+
         }
     }
 }
