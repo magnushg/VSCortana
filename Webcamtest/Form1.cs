0936aab0241fa2f83a553f7cd29ac9cef9aa9de0
Martin Gravråk <martin.gravrak@bouvet.no>
2016-04-02 09:33:41 +0200

    FirstWorking

--- Webcamtest/Form1.cs
+++ Webcamtest/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace Webcamtest
@@ -13,13 +14,30 @@ namespace Webcamtest
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            camcam.Start();
 
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
  
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var emotions = await camcam.GetEmotions();
+            if (emotions.Any())
+            {
+                listBox1.Items.Clear();
+                var programmer = emotions.First();
+                listBox1.Items.Add("Surprise: " + programmer.Scores.Surprise);
+                listBox1.Items.Add("Anger: " + programmer.Scores.Anger);
+                listBox1.Items.Add("Contempt: " + programmer.Scores.Contempt);
+                listBox1.Items.Add("Fear: " + programmer.Scores.Fear);
+                listBox1.Items.Add("Happiness: " + programmer.Scores.Happiness);
+                listBox1.Items.Add("Neutral: " + programmer.Scores.Neutral);
+                listBox1.Items.Add("Sadness: " + programmer.Scores.Sadness);
+                listBox1.Items.Add("Disgust: " + programmer.Scores.Disgust);
+            }
         }
     }
 }
